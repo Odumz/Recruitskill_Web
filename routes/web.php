c42297b383d2ef8088642fb82ebffe67bb214202
Odumosu Oluwashina <odumz20@gmail.com>
2019-12-17 14:42:41 +0100

    added landing

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/landing', function () {
+    return view('landing');
+});
+
 Auth::routes();
 
 // Route::get('user', 'UserController');
